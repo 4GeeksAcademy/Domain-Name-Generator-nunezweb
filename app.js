62c0a6e8f3dc7cd3bceda5a9a645880f906dcc95
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-05-19 19:57:56 +0000

    Add Domain Hack

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let domains = ['.es', '.com', '.org', 'domainHack']; // domainHack nos permite identificar cuando debemos ejecutar la lógica del dominio
 
-let domainGenerator = []
+let domainGenerator = [];
 
-for(let i = 0; i < pronoun.length; i++) {
-  for(let x = 0; x < adj.length; x++){
-    for(let y = 0; y < noun.length; y++){
-      for(let z = 0; z < domains.length; z++){
-        if(domains[z] === "domainHack"){ //verificamos si el dominio es domainHack para ejecutar la lógica
-          domainGenerator.push(pronoun[i] + adj[x] + noun[y].slice(0,-2)+"."+noun[y].slice(-2))
+for (let i = 0; i < pronoun.length; i++) {
+  for (let x = 0; x < adj.length; x++) {
+    for (let y = 0; y < noun.length; y++) {
+      for (let z = 0; z < domains.length; z++) {
+        if (domains[z] === 'domainHack') { //verificamos si el dominio es domainHack para ejecutar la lógica
+          domainGenerator.push(
+            pronoun[i] + adj[x] + noun[y].slice(0, -2) + '.' + noun[y].slice(-2) // aplicamos los cortes necesarios con .slice para crear la extensión de dominio nueva
+          );
+        } else {
+          domainGenerator.push(pronoun[i] + adj[x] + noun[y] + domains[z]);
         }
-        else {
-        domainGenerator.push(pronoun[i] + adj[x] + noun[y] + domains[z]);
-      }
       }
     }
-  }  
+  }
 }
 
 console.log(domainGenerator);
